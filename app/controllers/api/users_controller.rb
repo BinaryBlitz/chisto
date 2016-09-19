23553b564269e49aa2e0c18ab90d8b407e59ff20
SumLare <mrhard95@gmail.com>
2016-09-19 23:55:28 +0300

    API: Users

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,38 @@
+class API::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update]
+
+  def show
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render status: :created
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      head :ok
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.first
+  end
+
+  def user_params
+    params
+      .require(:user)
+      .permit(
+        :first_name, :last_name, :email, :phone_number, :address, :birthdate
+      )
+  end
+end
