c97385e06dea356275b23dfdbb1138f8768f92b1
SumLare <mrhard95@gmail.com>
2016-09-24 01:20:53 +0300

    Add uploaders to washing

--- app/models/washing.rb
+++ app/models/washing.rb
@@ -1,4 +1,9 @@
 class Washing < ApplicationRecord
   validates :category, inclusion: { in: %w(econom premium) }
   validates :name, :description, presence: true
+  validates :image, presence: true, unless: :logo?
+  validates :logo, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
+  mount_uploader :logo, LogoUploader
 end
