04e5b6d56673bdedeea90df69a402baae9873b6a
SumLare <mrhard95@gmail.com>
2016-09-24 17:31:04 +0300

    Create laundry

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919215153) do
+ActiveRecord::Schema.define(version: 20160924140539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "laundries", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.string   "description", null: false
+    t.string   "logo"
+    t.string   "image"
+    t.string   "category",    null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",   null: false
