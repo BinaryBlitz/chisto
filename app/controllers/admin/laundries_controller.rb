8dfff0d220681806fcdd21f59c2e1efd9924443f
SumLare <mrhard95@gmail.com>
2016-09-29 15:41:34 +0300

    Laundry admin (#8)
    * Set up bootstrap and slim
    
    * Set up carrierwave
    
    * Add uploads to gitignore
    
    * Add uploaders to laundry
    
    * Admin: Laundries
    
    * Admin: Laundry views
    
    * Laundry tests

---
+++ app/controllers/admin/laundries_controller.rb
@@ -0,0 +1,50 @@
+class Admin::LaundriesController < ApplicationController
+  before_action :set_laundry, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @laundries = Laundry.all
+  end
+
+  def show
+  end
+
+  def new
+    @laundry = Laundry.new
+  end
+
+  def create
+    @laundry = Laundry.new(laundry_params)
+
+    if @laundry.save
+      redirect_to admin_laundries_url, notice: 'Химчистка была успешно создана.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @laundry.update(laundry_params)
+      redirect_to admin_laundries_url, notice: 'Химчистка успешно обновлена.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @laundry.destroy
+    redirect_to admin_laundries_url, notice: 'Химчистка была успешно удалена.'
+  end
+
+  private
+
+  def set_laundry
+    @laundry = Laundry.find(params[:id])
+  end
+
+  def laundry_params
+    params.require(:laundry).permit(:name, :description, :category, :logo, :image)
+  end
+end
--- app/models/laundry.rb
+++ app/models/laundry.rb
@@ -1,4 +1,9 @@
 class Laundry < ApplicationRecord
   validates :category, inclusion: { in: %w(economy premium) }
   validates :name, :description, presence: true
+  validates :image, presence: true, unless: :logo?
+  validates :logo, presence: true, unless: :image?
+
+  mount_uploader :image, ImageUploader
+  mount_uploader :logo, LogoUploader
 end
