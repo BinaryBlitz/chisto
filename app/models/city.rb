069c1293d279344e01cac8f561fac6e9451028bb
Dan Kim <itsdanya@gmail.com>
2016-10-18 00:56:45 +0300

    Create cities

---
+++ app/models/city.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class City < ApplicationRecord
+  validates :name, presence: true
+end
---
+++ test/models/city_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: cities
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class CityTest < ActiveSupport::TestCase
+  setup do
+    @city = cities(:city)
+  end
+
+  test 'invalid without name' do
+    @city.name = nil
+    assert @city.invalid?
+  end
+end
