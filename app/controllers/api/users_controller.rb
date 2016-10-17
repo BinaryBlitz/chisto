3dd4d9237bc721e2be47abee9d204510e7ce8878
Dan Kim <itsdanya@gmail.com>
2016-10-18 01:11:35 +0300

    Add address fields to users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -33,7 +33,8 @@ class API::UsersController < API::APIController
     params
       .require(:user)
       .permit(
-        :first_name, :last_name, :email, :phone_number, :address, :birthdate
+        :first_name, :last_name, :email, :phone_number, :birthdate,
+        :city_id, :street_name, :house_number, :apartment_number, :notes
       )
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,17 @@
+city = City.create!(name: 'City')
+
 user = User.create!(
   first_name: 'Foo', last_name: 'Bar',
   phone_number: '+79998887766', email: 'foo@bar.com',
-  address: 'Address', birthdate: 20.years.ago
+  birthdate: 20.years.ago,
+  city: city, street_name: 'Street name',
+  house_number: '1a', apartment_number: 1, notes: 'Notes'
 )
 
 user.update!(api_token: 'foobar')
 
 category = Category.create!(
   name: 'Category',
-  description: 'Description'
+  description: 'Description',
+  icon: 'icon'
 )
