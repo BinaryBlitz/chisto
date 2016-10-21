fe39ecbb03c155b54aad6b54e78acc06f5f815c0
Dan Kim <itsdanya@gmail.com>
2016-10-21 16:08:50 +0300

    API: ratings

---
+++ app/views/api/ratings/_rating.jbuilder
@@ -0,0 +1,1 @@
+json.extract! rating, :id, :value, :content, :created_at
---
+++ test/controllers/api/ratings_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class API::RatingsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @rating = ratings(:rating)
+    @laundry = @rating.laundry
+  end
+
+  test 'should create rating' do
+    assert_difference 'Rating.count' do
+      post api_laundry_ratings_path(@laundry, api_token: api_token), params: {
+        rating: @rating.attributes
+      }
+    end
+
+    assert_response :created
+  end
+end
