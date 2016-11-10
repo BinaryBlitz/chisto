54429f5edba34298e0bcd736902a5e07b31a758f
Dan Kim <itsdanya@gmail.com>
2016-11-10 13:17:48 +0300

    Admin: cities

---
+++ app/controllers/admin/cities_controller.rb
@@ -0,0 +1,47 @@
+class Admin::CitiesController < Admin::AdminController
+  before_action :set_city, only: [:edit, :update, :destroy]
+
+  def index
+    @cities = City.all
+  end
+
+  def new
+    @city = City.new
+  end
+
+  def create
+    @city = City.new(city_params)
+
+    if @city.save
+      redirect_to admin_cities_path, notice: 'Город успешно создан'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @city.update(city_params)
+      redirect_to admin_cities_path, notice: 'Город успешно обновлен'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @city.destroy
+    redirect_to admin_cities_path, notice: 'Город успешно удален'
+  end
+
+  private
+
+  def set_city
+    @city = City.find(params[:id])
+  end
+
+  def city_params
+    params.require(:city).permit(:name)
+  end
+end
