3788ac93a4ecfabd5be0eea375243d8d3899985c
Dan Kim <itsdanya@gmail.com>
2016-11-13 01:26:33 +0300

    Admin: treatments

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,9 @@ Rails.application.routes.draw do
 
     scope module: :admin, as: :admin do
       resources :categories do
-        resources :items, shallow: true
+        resources :items, shallow: true do
+          resources :treatments, shallow: true
+        end
       end
 
       resources :cities do
