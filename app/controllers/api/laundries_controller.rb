ec3b1aa4b2edcf8095aaaaee69cdc694e9f409dc
Dan Kim <itsdanya@gmail.com>
2016-11-26 22:53:19 +0300

    API: fix laundries#index, render only laundries with schedules

--- app/controllers/api/laundries_controller.rb
+++ app/controllers/api/laundries_controller.rb
@@ -2,7 +2,7 @@ class API::LaundriesController < API::APIController
   before_action :set_city, only: [:index]
 
   def index
-    @laundries = Laundry.includes(:schedules, laundry_treatments: :treatment)
+    @laundries = Laundry.joins(:schedules).includes(:schedules, laundry_treatments: :treatment)
   end
 
   def show
