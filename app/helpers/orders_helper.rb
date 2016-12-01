9a276c64b2a753107bbe59be73dccc3048dd99e9
Dan Kim <itsdanya@gmail.com>
2016-12-01 23:02:54 +0300

    Partner: localize time

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,12 +1,12 @@
 module OrdersHelper
   def status_for_order(order)
-    content_tag(:span, class: ['tag', status_tag_class_for_order(order)]) do
-      status_tag_for_order(order)
-    end
+    Order.human_attribute_name("status.#{order.status}")
   end
 
   def status_tag_for_order(order)
-    Order.human_attribute_name("status.#{order.status}")
+    content_tag(:span, class: ['tag', status_tag_class_for_order(order)]) do
+      status_for_order(order)
+    end
   end
 
   def status_tag_class_for_order(order)
