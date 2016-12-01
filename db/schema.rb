be620b0cc9b3c62450005fa7a7dc9a8af0cb5c93
Dan Kim <itsdanya@gmail.com>
2016-12-01 23:35:38 +0300

    Add featured to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126202213) do
+ActiveRecord::Schema.define(version: 20161201203350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,13 @@ ActiveRecord::Schema.define(version: 20161126202213) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "name",        null: false
+    t.string   "name",                        null: false
     t.string   "description"
     t.string   "icon"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "color",       null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "color",                       null: false
+    t.boolean  "featured",    default: false
   end
 
   create_table "cities", force: :cascade do |t|
