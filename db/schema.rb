184b5a05becad6532a621461c5eb6df5ece4ae85
Dan Kim <itsdanya@gmail.com>
2016-12-15 08:12:19 +0300

    Order: total price (#50)
    * Add total price to orders
    
    * API: render total price in order partial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214013718) do
+ActiveRecord::Schema.define(version: 20161215050605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20161214013718) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
     t.string   "email"
+    t.integer  "total_price",                      null: false
     t.index ["laundry_id"], name: "index_orders_on_laundry_id", using: :btree
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
