ccba3a5869ead29daaa01567df28d525ed6071b5
Dan Kim <itsdanya@gmail.com>
2016-12-21 01:13:44 +0300

    Notifier: update options hash in GCM

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -37,7 +37,8 @@ class Notifier
   def push_android_notification
     gcm = GCM.new(Rails.application.secrets.gcm_api_key)
     registration_ids = [@device_token]
-    gcm.send(registration_ids, data: @options)
+    options = { notification: { title: 'Статус заказа изменён', body: @message }, data: @options }
+    gcm.send(registration_ids, options)
 
     Rails.logger.debug "#{Time.zone.now} Android notification: #{@message}, user: #{@user.id}"
   end
