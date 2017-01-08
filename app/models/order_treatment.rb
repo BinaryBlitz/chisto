a263f9b2674dea8bd7d89b0f215e414fc21974cd
Dan Kim <itsdanya@gmail.com>
2017-01-08 04:54:33 +0300

    Add price to order treatments

--- app/models/order_treatment.rb
+++ app/models/order_treatment.rb
@@ -7,9 +7,20 @@
 #  laundry_treatment_id :integer
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
+#  price                :integer          not null
 #
 
 class OrderTreatment < ApplicationRecord
   belongs_to :order_item, inverse_of: :order_treatments
   belongs_to :laundry_treatment
+
+  before_validation :set_price, on: :create
+
+  delegate :treatment, to: :laundry_treatment
+
+  private
+
+  def set_price
+    self.price = laundry_treatment.price
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107231135) do
+ActiveRecord::Schema.define(version: 20170108015042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -140,6 +140,7 @@ ActiveRecord::Schema.define(version: 20170107231135) do
     t.integer  "laundry_treatment_id"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "price",                null: false
     t.index ["laundry_treatment_id"], name: "index_order_treatments_on_laundry_treatment_id", using: :btree
     t.index ["order_item_id"], name: "index_order_treatments_on_order_item_id", using: :btree
   end
