5b64321220b13ce09cc1fc0c1f8fb886034b92ac
Dan Kim <itsdanya@gmail.com>
2017-01-18 20:01:52 +0300

    Create support requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113191026) do
+ActiveRecord::Schema.define(version: 20170118165242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -224,6 +224,14 @@ ActiveRecord::Schema.define(version: 20170113191026) do
     t.string   "content"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "support_requests", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "email",      null: false
+    t.text     "content",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "treatments", force: :cascade do |t|
