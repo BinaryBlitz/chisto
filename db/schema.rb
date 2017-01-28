b49d31c93363ff6b2e85efbee59d7dddb933dd51
Dan Kim <itsdanya@gmail.com>
2017-01-28 19:03:09 +0300

    Promo code: add unique index on code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128141009) do
+ActiveRecord::Schema.define(version: 20170128155602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -208,6 +208,7 @@ ActiveRecord::Schema.define(version: 20170128141009) do
     t.datetime "valid_until"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.index ["code"], name: "index_promo_codes_on_code", unique: true, using: :btree
     t.index ["laundry_id"], name: "index_promo_codes_on_laundry_id", using: :btree
     t.index ["promotion_id"], name: "index_promo_codes_on_promotion_id", using: :btree
   end
