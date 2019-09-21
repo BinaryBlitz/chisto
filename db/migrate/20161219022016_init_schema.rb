0feb26ff43a520438dc0d559216e95be2ab354c0
Dan Kim <itsdanya@gmail.com>
2019-09-22 00:06:53 +0300

    Specify migration version

--- db/migrate/20161219022016_init_schema.rb
+++ db/migrate/20161219022016_init_schema.rb
@@ -1,9 +1,9 @@
-class InitSchema < ActiveRecord::Migration
+class InitSchema < ActiveRecord::Migration[5.0]
   def up
-    
+
     # These are extensions that must be enabled in order to support this database
     enable_extension "plpgsql"
-    
+
     create_table "admins", force: :cascade do |t|
       t.string   "email",                  default: "", null: false
       t.string   "encrypted_password",     default: "", null: false
@@ -20,7 +20,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
       t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
     end
-    
+
     create_table "categories", force: :cascade do |t|
       t.string   "name",                          null: false
       t.string   "description"
@@ -32,7 +32,7 @@ class InitSchema < ActiveRecord::Migration
       t.integer  "items_count",   default: 0
       t.string   "items_preview", default: [],                 array: true
     end
-    
+
     create_table "cities", force: :cascade do |t|
       t.string   "name",       null: false
       t.datetime "created_at", null: false
@@ -40,7 +40,7 @@ class InitSchema < ActiveRecord::Migration
       t.float    "latitude",   null: false
       t.float    "longitude",  null: false
     end
-    
+
     create_table "items", force: :cascade do |t|
       t.integer  "category_id"
       t.string   "name",                        null: false
@@ -51,7 +51,7 @@ class InitSchema < ActiveRecord::Migration
       t.boolean  "use_area",    default: false
       t.index ["category_id"], name: "index_items_on_category_id", using: :btree
     end
-    
+
     create_table "laundries", force: :cascade do |t|
       t.string   "name",                                    null: false
       t.string   "description",                             null: false
@@ -87,7 +87,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["email"], name: "index_laundries_on_email", unique: true, using: :btree
       t.index ["reset_password_token"], name: "index_laundries_on_reset_password_token", unique: true, using: :btree
     end
-    
+
     create_table "laundry_items", force: :cascade do |t|
       t.integer  "laundry_id"
       t.integer  "item_id"
@@ -97,7 +97,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["item_id"], name: "index_laundry_items_on_item_id", using: :btree
       t.index ["laundry_id"], name: "index_laundry_items_on_laundry_id", using: :btree
     end
-    
+
     create_table "laundry_treatments", force: :cascade do |t|
       t.integer  "laundry_id"
       t.integer  "treatment_id"
@@ -107,7 +107,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["laundry_id"], name: "index_laundry_treatments_on_laundry_id", using: :btree
       t.index ["treatment_id"], name: "index_laundry_treatments_on_treatment_id", using: :btree
     end
-    
+
     create_table "line_items", force: :cascade do |t|
       t.integer  "order_id"
       t.integer  "laundry_treatment_id"
@@ -122,7 +122,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["order_id"], name: "index_line_items_on_order_id", using: :btree
       t.index ["treatment_id"], name: "index_line_items_on_treatment_id", using: :btree
     end
-    
+
     create_table "orders", force: :cascade do |t|
       t.integer  "user_id"
       t.integer  "laundry_id"
@@ -141,7 +141,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["laundry_id"], name: "index_orders_on_laundry_id", using: :btree
       t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
     end
-    
+
     create_table "partner_applications", force: :cascade do |t|
       t.string   "representative_name", null: false
       t.string   "laundry_name",        null: false
@@ -152,7 +152,7 @@ class InitSchema < ActiveRecord::Migration
       t.datetime "created_at",          null: false
       t.datetime "updated_at",          null: false
     end
-    
+
     create_table "payments", force: :cascade do |t|
       t.integer  "order_id"
       t.integer  "amount",                      null: false
@@ -162,7 +162,7 @@ class InitSchema < ActiveRecord::Migration
       t.datetime "updated_at",                  null: false
       t.index ["order_id"], name: "index_payments_on_order_id", using: :btree
     end
-    
+
     create_table "ratings", force: :cascade do |t|
       t.integer  "user_id"
       t.integer  "laundry_id"
@@ -174,7 +174,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["laundry_id"], name: "index_ratings_on_laundry_id", using: :btree
       t.index ["user_id"], name: "index_ratings_on_user_id", using: :btree
     end
-    
+
     create_table "schedules", force: :cascade do |t|
       t.integer  "laundry_id"
       t.integer  "day_of_the_week", null: false
@@ -184,7 +184,7 @@ class InitSchema < ActiveRecord::Migration
       t.datetime "updated_at",      null: false
       t.index ["laundry_id"], name: "index_schedules_on_laundry_id", using: :btree
     end
-    
+
     create_table "treatments", force: :cascade do |t|
       t.integer  "item_id"
       t.string   "name",        null: false
@@ -193,7 +193,7 @@ class InitSchema < ActiveRecord::Migration
       t.datetime "updated_at",  null: false
       t.index ["item_id"], name: "index_treatments_on_item_id", using: :btree
     end
-    
+
     create_table "users", force: :cascade do |t|
       t.string   "first_name",                   null: false
       t.string   "last_name",                    null: false
@@ -215,7 +215,7 @@ class InitSchema < ActiveRecord::Migration
       t.index ["api_token"], name: "index_users_on_api_token", unique: true, using: :btree
       t.index ["city_id"], name: "index_users_on_city_id", using: :btree
     end
-    
+
     create_table "verification_tokens", force: :cascade do |t|
       t.string   "token",                        null: false
       t.string   "phone_number",                 null: false
@@ -225,7 +225,7 @@ class InitSchema < ActiveRecord::Migration
       t.datetime "updated_at",                   null: false
       t.index ["token"], name: "index_verification_tokens_on_token", unique: true, using: :btree
     end
-    
+
     add_foreign_key "items", "categories"
     add_foreign_key "laundry_items", "items"
     add_foreign_key "laundry_items", "laundries"
